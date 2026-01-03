32c73fd01148dd01d63f43294d4a5716a2273181
vjagadeesh924-blip <vjagadeesh924@gmail.com>
2026-01-03 13:35:48 +0530

    Add files via upload

---
+++ Relational Operators.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
+# user input 
+
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print(x == y)
+print(x > y)
+print(x < y)
+
